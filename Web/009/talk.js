e01058e6ac9f77770ca697af1a21184dc00ee4f7
ElonmuskLeebibgoin <145531174+ElonmuskLeebibgoin@users.noreply.github.com>
2024-12-02 15:36:36 +0800

    Update talk.js

--- Web/009/talk.js
+++ Web/009/talk.js
@@ -1,6 +1,6 @@
 $(function() {
     $('#yes').click(function(event) {
-        modal('我就知道小宝宝一定会愿意~(^_^)', function() {
+        modal('我就知道姐宝一定会愿意~(^_^)', function() {
             $('.page_one').addClass('hide');
             $('.page_two').removeClass('hide');
             // typeWrite();
@@ -22,7 +22,7 @@ function B() {
 }
 
 function C() {
-    modal('请宝宝不要拒绝我嘛~', D);
+    modal('请姐姐不要拒绝我嘛~', D);
 }
 
 function D() {
